469ac4f452f1f1e5087d28ccfe5a3319681b93c2
STANISLAV-Ilinskiy <93650229+STANISLAV-Ilinskiy@users.noreply.github.com>
2023-12-20 22:44:49 +0600

    Add files via upload

---
+++ Zadanie 2/Zadanie 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+ * Даны два неотрицательных числа m и n.*/
+
+namespace Zadanie_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение M:");
+            int M = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int result = 0;
+
+            if (M < 0 || N < 0)
+            {
+                Console.WriteLine("Ошибка: значения M и N не могут быть отрицательными.");
+                Console.WriteLine();
+                return;
+            }
+            else
+            {
+                result = Ackermann(M, N);
+            }
+                          
+            
+            Console.WriteLine($"Результат функции Аккермана для M={M} и N={N} равен {result}");
+            Console.WriteLine();
+        }
+
+        static int Ackermann(int M, int N)
+        {
+            if (M == 0)
+            {
+                return N + 1;
+            }
+            else if (N == 0)
+            {
+                return Ackermann(M - 1, 1);
+            }
+            else
+            {
+                return Ackermann(M - 1, Ackermann(M, N - 1));
+            }
+
+        }
+    }
+}
---
+++ Zadanie 3/Zadanie 3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Задайте произвольный массив. Выведете его элементы, начиная с конца.
+ * Использовать рекурсию, не использовать циклы.*/
+
+namespace Zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = GenerateRandomArray(10);
+            Console.WriteLine("\t Исходный массив: ");
+            Console.WriteLine();
+            PrintArray(array);
+
+            Console.WriteLine();
+
+            Console.WriteLine("\t Реверс массива: ");
+            Console.WriteLine();
+            PrintArrayReverse(array, array.Length - 1);
+            Console.WriteLine();
+
+        }
+
+        // Функция рандомного заполнения массива
+        static int[] GenerateRandomArray(int size)
+        {
+            int[] randomArray = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                randomArray[i] = rnd.Next(10, 100);
+            }
+            
+            return randomArray;
+        }
+
+        //Функция вывода массива
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            
+
+        }
+
+        //Рекурсивная функция реверса массива
+        static void PrintArrayReverse(int[] array, int index)
+        {
+            if (index >= 0)
+            {
+                Console.Write(array[index] + " ");
+                PrintArrayReverse(array, index - 1);
+            }
+        }
+    }
+}
